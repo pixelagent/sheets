fb9e73b983d697392949106cae59b35e8ac1d0dc
pixelagent <nigel.morris@gmail.com>
2018-07-24 15:41:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -45,16 +45,20 @@ var code = '1ciPq3VfxUv3ucttkMPzNXNR1NLKA1JrOq1tGiLg2CsI'
           L.marker([data.longitude, data.latitude], {icon: icon})
           .addTo(map)
 	  .bindPopup(popupContent,{closeButton: false, minWidth: 300});
-				   
+	  
+	  /*			   
           .bindPopup("<strong style='color: #84b819'>" + 
 		     data.newsroom + "</strong><br>" + 
 		     data.company + " | " + 
 		     data.city + "<br>Head: " + 
 		     data.head).openPopup();
-	      
+	   */   
 	      
 	// Create custom popup content
-       	// var popupContent =  '"<strong style='color: #84b819'>" + data.newsroom + "</strong><br>" +  data.company + " | " + data.city + "<br>Head: " + data.head';
+       var popupContent =  '"<strong style='color: #84b819'>" + 
+       		data.newsroom + "</strong><br>" +  
+	        data.company + " | " + data.city + "<br>Head: " + 
+	        data.head';
 
         
       }
