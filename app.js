617c31cc2007e46d6f03bf74cdecfeba41d7df8d
pixelagent <nigel.morris@gmail.com>
2018-07-24 16:25:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function myFunction() {
-  var map = L.map('map').setView([51.101516, 10.313446], 6);
+  var map = L.map('map').setView([51.101516, 10.313446], 12);
   if (map.tap) map.tap.disable();
   L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
