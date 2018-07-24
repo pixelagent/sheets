801b525c187c9e851bebedc07978fc5038702e93
pixelagent <nigel.morris@gmail.com>
2018-07-24 13:24:10 +0100

    Update list.js

--- list.js
+++ list.js
@@ -1,22 +1,38 @@
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script> 
-<script type="text/javascript"> 
-$(document).ready(function() {	
-//source file is https://docs.google.com/spreadsheet/ccc?key=0Ak0qDiMLT3XddHlNempadUs1djdkQ0tFLWF6ci1rUUE	
-$(function listBooks() {	
-$.getJSON( "https://spreadsheets.google.com/feeds/list/0Ak0qDiMLT3XddHlNempadUs1djdkQ0tFLWF6ci1rUUE/od6/public/values?alt=json-in-script&callback=?",
-	function (data) {	
-		$('div#book-list').append('<ul class="items"></ul>');
-		$.each(data.feed.entry, function(i,entry) {	
-			var item = '<span style="display:none">' + entry.id.$t + '</span>';	
-			item += '<img src="http://covers.openlibrary.org/b/isbn/' + entry.gsx$isbn.$t + '-S.jpg"/>';
-			item += '<span class="meta"><a href="http://www.worldcat.org/isbn/' + entry.gsx$isbn.$t + '">' + entry.title.$t + '</a>';	
-			item += '<br/>Author: ' + entry.gsx$author.$t;	
-			if (entry.gsx$notes.$t) {	
-				item += '<br/>Description: ' + entry.gsx$notes.$t;	
-			}	
-			$('.items').append('<li>' + item + '</span></li>');	
-			});
-		});
-	});
-});
+<script type="text/javascript">
+
+//standard opening so that the script starts after page is loaded.
+
+$(document).ready(function() {   
+
+//Begin listBooks function.
+	$(function listBooks() {   
+ 
+//Begin getJSON function. Make sure the URL has the correct key and ID. Name your div well, you will need it for the html part.  The name you choose for the class will be used in the CSS and therefore can be changed to something meaningful.
+		$.getJSON( "https://spreadsheets.google.com/feeds/list/0AuwEG4hnNDDYdDJQR2xZTnJkUVd6RXZINEZKY1VtT3c/od6/public/values?alt=json-in-script&callback=?", function (data) { 
+		$('div#philosophy').append('<ul class="items"></ul>');
+
+//Begin data.feed.entry function. This is where you add the Open Library book covers and link to your OPAC,
+		$.each(data.feed.entry, function(i,entry) { var
+
+				item = '<span style="display:none">' + entry.id.$t + '</span>';    
+
+				item += '<a href="http://whatcom.library.ctc.edu/vwebv/search?searchArg=' + entry.gsx$isbn.$t + '&searchCode=GKEY^*&limitTo=none&recCount=50&searchType=1&page.search.search.button=Search">' + '<img src="http://covers.openlibrary.org/b/isbn/' + entry.gsx$isbn.$t + '-S.jpg" />' + entry.gsx$callnumber.$t + '</a>';                
+
+
+				item += '<br/>' + entry.gsx$title.$t;    
+
+           		if (entry.gsx$notes.$t) {item += '<br/>Description: ' + entry.gsx$notes.$t; }  
+
+           	$('.items').append('<li>' + item + '</span></li>'); 
+   
+
+   			}); //ends data.feed.entry function 
+
+   			}); //ends getJSON function
+
+			}); //ends listBooks function
+
+			}); //ends document.ready function
+
 </script>
+ 
